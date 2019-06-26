09a308a497a63f9cf77575cff05adede624df31e
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-26 07:34:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var array=[];
 function theBeatlesPlay(arrayMusicians,arrayInstruments){
   for(var i =0; i<arrayMusicians.length;i++){
     if(i===0){
-      
+        array.push(`${arrayMusicians[i]} plays ${arrayMusicians[i]}`);
     }
-    array.push(`${arrayMusicians[i]} plays ${arrayMusicians[i]}`);
+  
   }
 }
